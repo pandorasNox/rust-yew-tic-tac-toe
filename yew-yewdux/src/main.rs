8f63db2d9d5e5a7f0ecff9f0845075c7ecddbd0c
pandorasNox <itspandorasnox@gmail.com>
2022-08-27 16:38:49 +0200

    yew-yewdux: add win handling

--- yew-yewdux/src/main.rs
+++ yew-yewdux/src/main.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{fmt, rc::Rc};
 
 use yew::{function_component, html, prelude::*, virtual_dom::VNode};
 use yewdux::prelude::*;
@@ -13,6 +13,13 @@ enum Piece {
     #[default]
     X,
     O,
+}
+impl fmt::Display for Piece {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+        // or, alternatively:
+        // fmt::Debug::fmt(self, f)
+    }
 }
 
 type TTTGrid = [[Option<Piece>; 3]; 3];
@@ -37,6 +44,111 @@ impl State {
             Piece::O => self.current_player = Piece::X,
         }
     }
+
+    fn has_won(&self) -> (bool, Option<Piece>) {
+        let grid = self.grid;
+        let mut x_count = 0;
+        let mut o_count = 0;
+
+        //check rows
+        for i_row in 0..grid.len() {
+            for i_col in 0..grid[i_row].len() {
+                let elem = grid[i_row][i_col];
+                match elem {
+                    None => {}
+                    Some(Piece::X) => x_count += 1,
+                    Some(Piece::O) => o_count += 1,
+                }
+            }
+
+            match (x_count, o_count) {
+                (3, _) => {
+                    return (true, Some(Piece::X));
+                }
+                (_, 3) => {
+                    return (true, Some(Piece::O));
+                }
+                _ => {
+                    x_count = 0;
+                    o_count = 0;
+                }
+            }
+        }
+
+        //check columns
+        for i_col in 0..grid[0].len() {
+            for i_row in 0..grid[i_col].len() {
+                let elem = grid[i_row][i_col];
+                match elem {
+                    None => {}
+                    Some(Piece::X) => x_count += 1,
+                    Some(Piece::O) => o_count += 1,
+                }
+            }
+
+            match (x_count, o_count) {
+                (3, _) => {
+                    return (true, Some(Piece::X));
+                }
+                (_, 3) => {
+                    return (true, Some(Piece::O));
+                }
+                _ => {
+                    x_count = 0;
+                    o_count = 0;
+                }
+            }
+        }
+
+        //check diagonals top_left_to_bottom_right
+        for i_row in 0..grid.len() {
+            let elem = grid[i_row][i_row];
+            match elem {
+                None => {}
+                Some(Piece::X) => x_count += 1,
+                Some(Piece::O) => o_count += 1,
+            }
+        }
+
+        match (x_count, o_count) {
+            (3, _) => {
+                return (true, Some(Piece::X));
+            }
+            (_, 3) => {
+                return (true, Some(Piece::O));
+            }
+            _ => {
+                x_count = 0;
+                o_count = 0;
+            }
+        }
+
+        //check diagonals top_right_to_bottom_left
+        for i_row in 0..grid.len() {
+            let last_i = grid.len() - 1;
+            let elem = grid[i_row][last_i - i_row];
+            match elem {
+                None => {}
+                Some(Piece::X) => x_count += 1,
+                Some(Piece::O) => o_count += 1,
+            }
+        }
+
+        match (x_count, o_count) {
+            (3, _) => {
+                return (true, Some(Piece::X));
+            }
+            (_, 3) => {
+                return (true, Some(Piece::O));
+            }
+            _ => {
+                x_count = 0;
+                o_count = 0;
+            }
+        }
+
+        return (false, None);
+    }
 }
 
 enum Msg {
@@ -53,6 +165,11 @@ impl Reducer<State> for Msg {
                 return Rc::new(State::new());
             }
             Msg::PiecePlacement(i_row, i_col) => {
+                let (has_won, _) = game_state.has_won();
+                if has_won {
+                    return state;
+                }
+
                 match game_state.grid[*i_row][*i_col] {
                     None => {
                         game_state.grid[*i_row][*i_col] = Some(game_state.current_player);
@@ -70,13 +187,31 @@ impl Reducer<State> for Msg {
 
 #[function_component(App)]
 fn app() -> Html {
-    let (_, dispatch) = use_store::<State>();
+    let (state, dispatch) = use_store::<State>();
     let reset = dispatch.apply_callback(move |_| Msg::Reset);
+    let (has_won, winning_piece) = state.has_won();
+    let winner = match winning_piece {
+        Some(piece) => piece.to_string(),
+        _ => "".to_string(),
+    };
 
     html! {
         <>
             <h1>{"Tic Tac Toe"}</h1>
-            <Grid />
+            <div class="flex justify-center">
+                <div>
+                    <h2>
+                        {"Winner is: "}
+                        if has_won {
+                            {winner}
+                        } else {
+                            {"..."}
+                        }
+                    </h2>
+
+                    <Grid />
+                </div>
+            </div>
             <div class="flex justify-center">
                 <button onclick={reset} class="border rounded p-4">{"Reset"}</button>
             </div>
@@ -124,10 +259,8 @@ fn grid() -> Html {
     let elems_as_html = elems.into_iter().collect::<Html>();
 
     html! {
-        <div class="flex justify-center">
-            <div class="w-80 h-80 grid grid-cols-3 gap-2 border justify-items-center">
-                {elems_as_html}
-            </div>
+        <div class="w-80 h-80 grid grid-cols-3 gap-2 border justify-items-center">
+            {elems_as_html}
         </div>
     }
 }
